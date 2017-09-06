8a15218e9389956f4593828d257fe44de0799be0
Daniel Levett <dlevett@hotmail.co.uk>
2017-09-06 22:03:19 +0100

    List country listing in sitemap, and include all images for posts.

--- routes/sitemapRoute.js
+++ routes/sitemapRoute.js
@@ -1,23 +1,35 @@
 const travelPosts = require('../data/travelPosts');
 
+const getParentPaths = (path) => {
+	const parent = path.split('/').slice(0, -1).join('/');
+
+	if (parent === '') {
+		return [];
+	}
+
+	return getParentPaths(parent).concat(parent);
+};
+
+const getAllParentRoutes = (items) => {
+	const paths = items.reduce((acc, item) => {
+		return acc.concat(getParentPaths(item.href));
+	}, []);
+	const uniquePaths = new Set(paths);
+
+	// TODO: Change 'href' to 'path
+	return Array.from(uniquePaths).map(href => ({href}));
+};
+
 module.exports = (router) => {
 
 	router.get('/sitemap.xml', async (ctx) => {
-		const homePage = {href: '/'};
-		const travelPostsPages = travelPosts.map(({href, mainImage}) => ({
-			href,
-			imageSrc: mainImage.src
-		}));
-		const allPages = []
-			.concat(homePage)
-			.concat(travelPostsPages)
-			.map(({href, imageSrc, ...otherProps}) => ({
-				href: ctx.origin + href,
-				imageSrc: imageSrc ? ctx.origin + imageSrc : undefined,
-				...otherProps
-			}));
-
-		await ctx.render('sitemap', {pages: allPages});
+		await ctx.render('sitemap', {
+			pages: []
+				.concat({href: '/'})
+				.concat(getAllParentRoutes(travelPosts))
+				.concat(travelPosts),
+			origin: ctx.origin
+		});
 		ctx.type = 'text/xml';
 	});
 
