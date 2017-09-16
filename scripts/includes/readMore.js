3770fc8cad54a76ed890f7c67ea1b2a6890a22c2
Daniel Levett <dlevett@hotmail.co.uk>
2017-09-16 15:50:53 +0100

    Fix bug where “read more” text is already expanded on homepage when using breadcrumb

--- scripts/includes/readMore.js
+++ scripts/includes/readMore.js
@@ -1,5 +1,8 @@
+var instantclick = require('instantclick');
+
 function init() {
 	var READMORE_MAX_WIDTH = 500;
+	var SELECTOR = '[data-readmore-summary]';
 
 	function $(selector) {
 		var elems = document.querySelectorAll(selector);
@@ -7,27 +10,29 @@ function init() {
 	}
 
 	function initReadmore() {
-		var SELECTOR = '[data-readmore-summary]';
+		if (window.innerWidth > READMORE_MAX_WIDTH) {
+			return;
+		}
 
-		document.addEventListener('DOMContentLoaded', function () {
-			$(SELECTOR).forEach(function (elem) {
-				var button = '<p><button>Read more...</button></p>';
-				elem.dataset.readmoreFull = elem.innerHTML;
-				elem.innerHTML = elem.dataset.readmoreSummary + button;
-			});
+		$(SELECTOR).forEach(function (elem) {
+			var button = '<p><button>Read more...</button></p>';
+			elem.dataset.readmoreFull = elem.innerHTML;
+			elem.innerHTML = elem.dataset.readmoreSummary + button;
 		});
-
-		document.addEventListener('click', function (event) {
-			var wrapper = event.target.closest(SELECTOR);
-			if (wrapper && wrapper.dataset.readmoreFull) {
-				wrapper.innerHTML = wrapper.dataset.readmoreFull;
-			}
-		}, false);
 	}
 
-	if (window.innerWidth <= READMORE_MAX_WIDTH) {
+	document.addEventListener('click', function (event) {
+		var wrapper = event.target.closest(SELECTOR);
+		if (wrapper && wrapper.dataset.readmoreFull) {
+			wrapper.innerHTML = wrapper.dataset.readmoreFull;
+		}
+	}, false);
+
+	instantclick.on('change', function() {
 		initReadmore();
-	}
+	});
+
+	initReadmore();
 }
 
 module.exports = {init: init};
