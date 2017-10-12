62b38fe15bb5e5ffd1126783be3f4e43aa9d1e17
Daniel Levett <dlevett@hotmail.co.uk>
2017-10-12 20:55:51 +0100

    Use CSS for “Read more…” functionality

--- scripts/includes/readMore.js
+++ scripts/includes/readMore.js
@@ -1,31 +1,11 @@
-import instantclick from 'instantclick';
-
-const READMORE_MAX_WIDTH = 500;
-const SELECTOR = '[data-readmore-summary]';
-
-const hideMore = () => {
-	if (window.innerWidth > READMORE_MAX_WIDTH) return;
-
-	const elems = Array.prototype.slice.call(
-		document.querySelectorAll(SELECTOR)
-	);
-
-	elems.forEach((elem) => {
-		const button = '<p><button>Read more...</button></p>';
-		elem.dataset.readmoreFull = elem.innerHTML;
-		elem.innerHTML = elem.dataset.readmoreSummary + button;
-	});
-};
-
-const showMore = (event) => {
-	const wrapper = event.target.closest(SELECTOR);
-	if (wrapper && wrapper.dataset.readmoreFull) {
-		wrapper.innerHTML = wrapper.dataset.readmoreFull;
-	}
-};
-
 export const init = () => {
-	hideMore();
-	document.addEventListener('click', showMore, false);
-	instantclick.on('change', hideMore);
+	document.addEventListener(
+		'click',
+		({target}) => {
+			if (target.classList.contains('js-readmore-button')) {
+				target.parentNode.classList.add('js-readmore-active');
+			}
+		},
+		false
+	);
 };
