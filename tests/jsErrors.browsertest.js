4ddc34ed57d42c6ee6bb8df862a35c3a81a68084
Daniel Levett <dlevett@hotmail.co.uk>
2017-10-22 20:49:24 +0100

    Trigger features in tests to check for errors

--- tests/jsErrors.browsertest.js
+++ tests/jsErrors.browsertest.js
@@ -9,7 +9,18 @@ describe('JavaScript errors', () => {
 		let i = UNIQUE_PAGE_URLS.length;
 		while (i--) {
 			await page.goto(UNIQUE_PAGE_URLS[i]);
-			const errors = await page.evaluate(() => window.ERRORS);
+			const errors = await page.evaluate(() => {
+				// Setup
+				const photoswipe = document.querySelector('.js-photoswipe');
+				const readmore = document.querySelector('.js-readmore-button');
+
+				// If features exist on page, trigger them to check they do not
+				// cause errors.
+				if (photoswipe) photoswipe.click();
+				if (readmore) readmore.click();
+
+				return window.ERRORS;
+			});
 			expect(errors.length).toBe(0);
 		}
 
