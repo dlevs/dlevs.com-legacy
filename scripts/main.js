bfa88aae5b45115e38a1f6fef93ba1883422e2c7
Daniel Levett <dlevett@hotmail.co.uk>
2017-10-28 10:44:03 +0100

    Remove swipe to navigate and unused code

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,9 @@ import 'lazysizes';
 import { init as initInstantclick } from 'instantclick';
 import { init as initReadMore } from './includes/readMore';
 import { init as initPhotoswipe } from './includes/photoswipe';
-import { init as initHandleDisabledLinks } from './includes/handleDisabledLinks';
-import { init as initSwipeToNavigate } from './includes/swipeToNavigate';
 import { init as initGoogleAnalytics } from './includes/googleAnalytics';
 
 initInstantclick();
 initReadMore();
 initPhotoswipe();
-initHandleDisabledLinks();
-initSwipeToNavigate();
 initGoogleAnalytics();
