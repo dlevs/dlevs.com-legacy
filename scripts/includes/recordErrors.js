2a72c37c6880f90cb3aca18201a305a18cdf45f4
Daniel Levett <dlevett@hotmail.co.uk>
2017-11-25 20:20:14 +0000

    Remove unused file

--- scripts/includes/recordErrors.js
+++
@@ -1,9 +0,0 @@
-// This module needs to be imported first in order to catch errors.
-window.ERRORS = [];
-window.addEventListener('error', ({
-	message, filename, lineno, colno,
-}) => {
-	window.ERRORS.push({
-		message, filename, lineno, colno,
-	});
-});
