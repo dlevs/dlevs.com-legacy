c0655dc4e896825c5f162a4bb15d062ed9344016
Daniel Levett <dlevett@hotmail.co.uk>
2018-02-26 22:27:37 +0000

    Validate sitemap pages

--- routes/sitemapController.js
+++ routes/sitemapController.js
@@ -1,8 +1,22 @@
 'use strict';
 
 const Router = require('koa-router');
+const assert = require('assert');
+
+const validatePages = (pages) => {
+	pages.forEach(({ name, path, posts }) => {
+		assert(typeof name === 'string', 'Page name must be a string');
+		assert(typeof path === 'string', 'Page path must be a string');
+
+		// TODO: Change "posts" to "pages"
+		if (posts) {
+			validatePages(posts);
+		}
+	});
+};
 
 module.exports = ({ pages = [], breadcrumbRoot }) => {
+	validatePages(pages);
 	const breadcrumb = breadcrumbRoot.append({ name: 'Sitemap', slug: 'sitemap' });
 	const getRenderVariables = ctx => ({
 		pages,
@@ -10,7 +24,6 @@ module.exports = ({ pages = [], breadcrumbRoot }) => {
 		origin: ctx.origin,
 		meta: { title: breadcrumb.currentPage.name },
 	});
-
 	const controllers = {
 		xml: async (ctx) => {
 			await ctx.render('sitemapXml.pug', getRenderVariables(ctx));
