87caadc02421404d13a3187324f1ec9a5322c8c8
Daniel Levett <dlevett@hotmail.co.uk>
2018-07-08 20:57:16 +0100

    Improve test coverage and make cache time more configurable

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const { SECONDS_IN_A_DAY } = require('../lib/constants');
+
 exports.setProcessEnv = () => {
-	process.env.NODE_ENV = 'production';
+	process.env.NODE_ENV = 'development';
 };
 exports.PORT = 3000;
 exports.GOOGLE_ANALYTICS_ID = 'UA-57421315-4';
 exports.IS_BEHIND_PROXY = true;
-exports.HOSTNAME = 'dlevs.com';
-exports.ORIGIN = `https://${exports.HOSTNAME}`;
+exports.HOSTNAME = `localhost:${exports.PORT}`;
+exports.ORIGIN = `http://${exports.HOSTNAME}`;
+exports.STATIC_ASSET_MAX_AGE_IN_SECONDS = 0 * SECONDS_IN_A_DAY;
--- config/config.test.js
+++ config/config.test.js
@@ -10,6 +10,7 @@ const configTest = ({
 	IS_BEHIND_PROXY,
 	HOSTNAME,
 	ORIGIN,
+	STATIC_ASSET_MAX_AGE_IN_SECONDS,
 }) => {
 	test('setProcessEnv()', () => {
 		expect(typeof setProcessEnv).toBe('function');
@@ -24,6 +25,8 @@ const configTest = ({
 		expect(typeof IS_BEHIND_PROXY).toBe('boolean');
 		expect(typeof HOSTNAME).toBe('string');
 		expect(typeof ORIGIN).toBe('string');
+		expect(typeof STATIC_ASSET_MAX_AGE_IN_SECONDS).toBe('number');
+		expect(STATIC_ASSET_MAX_AGE_IN_SECONDS).toBeGreaterThanOrEqual(0);
 	});
 };
 
@@ -33,4 +36,24 @@ describe('config', () => {
 
 describe('sample config', () => {
 	configTest(sampleConfig);
+});
+
+describe('index', () => {
+	beforeEach(() => {
+		process.env.NODE_ENV = 'test';
+		jest.resetModules();
+	});
+	test('is test config when in test mode', () => {
+		// eslint-disable-next-line global-require
+		expect(require('../config')).toBe(require('./config.sample.js'));
+	});
+	describe('is real config when not in test mode', () => {
+		['production', 'development', 'somesillyvalue'].forEach((environment) => {
+			test(environment, () => {
+				process.env.NODE_ENV = environment;
+				// eslint-disable-next-line global-require
+				expect(require('../config')).toBe(require('./config.js'));
+			});
+		});
+	});
 });
